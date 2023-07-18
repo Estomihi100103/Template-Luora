2e410478c93a502556c05595255cfc24af8ff870
Samuel Sihotang <iss21041@students.del.ac.id>
2023-07-18 15:54:28 +0700

    Update Follow

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,16 @@ namespace App\Models;
 
 use App\Models\Comment;
 use Jcc\LaravelVote\Traits\Voter;
-use Overtrue\LaravelFollow\Followable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Overtrue\LaravelFollow\Traits\Follower;
+use Overtrue\LaravelFollow\Traits\Followable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, Follower, Followable;
 
     protected $guarded = [];
 
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -8,23 +8,23 @@ use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        //admin
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'name_slug' => 'admin',
-            'email' => 'admin@gmail.com',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-            'role' => 'admin',
-            'avatar' => 'https://ui-avatars.com/api/?name=admin&background=868e96&color=fff',
-        ]);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    //admin
+    DB::table('users')->insert([
+      'name' => 'admin',
+      'name_slug' => 'admin',
+      'email' => 'admin@gmail.com',
+      'email_verified_at' => now(),
+      'password' => bcrypt('admin@gmail.com'), // password
+      'remember_token' => Str::random(10),
+      'role' => 'admin',
+      'avatar' => 'https://ui-avatars.com/api/?name=admin&background=868e96&color=fff',
+    ]);
+  }
 }
